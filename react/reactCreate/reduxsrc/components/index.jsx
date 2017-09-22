8b52e8bcd5a845cd4f0821d49430f3cfc31865dc
guang <837990335@qq.com>
2017-09-22 10:01:05 +0800

    Sfs

--- react/reactCreate/reduxsrc/components/index.jsx
+++ react/reactCreate/reduxsrc/components/index.jsx
@@ -1,27 +1,43 @@
 import React, { Component } from 'react'
-import {store} from "../tool/index.js"
-import Child from "./child.jsx"
+// import {store} from "../tool/index.js"
+
+
+import store from "../tool/redux/redux.js"
+import Tm_index from "./tm/tm_index.jsx"
+import Zfb_index from "./zfb/zfb_index.jsx"
+
+
+import {add_c} from "../tool/redux/tm_m/actions.js"
+// import Child from "./child.jsx"
 class Index extends Component{
   constructor(props){
         super(props)
-        this.listenfn=this.listenfn.bind(this)
-        this.state={
-          num:store.getState().num
-        }
-        store.subscribe(this.listenfn)
+        console.log(this.props)
+        // this.listenfn=this.listenfn.bind(this)
+        // console.log(store.getState())
+        // this.state={
+        //   tm:store.getState().tm.count,
+        //   zfb:store.getState().zfb.num
+        // }
+        // store.subscribe(this.listenfn)
         
       
   }
-  listenfn(){
-    this.setState({
-      num:store.getState().num
-    })
-  }
+  
+  // listenfn(){
+  //   this.setState({
+  //      tm:store.getState().tm.count,
+  //         zfb:store.getState().zfb.num
+  //   })
+  // }
+
   render(){
     return (
       <div>
-          <span>{this.state.num}</span>
-          <Child></Child>
+          <p>{this.props.tm.count}</p>
+          <p>{this.props.zfb.num}</p>
+          <Tm_index fn={(text)=>{this.props.dispatch(add_c(text))}}></Tm_index>
+          <Zfb_index prop={this.props}></Zfb_index>
       </div>
     )
   }
--- react/reactCreate/reduxsrc/main.js
+++ react/reactCreate/reduxsrc/main.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
 import ReactDOM from "react-dom"
+import { connect, Provider } from "react-redux"
+import store from "./tool/redux/redux.js"
 
 import Index from "./components/index.jsx"
-ReactDOM.render( <
-    Index / > ,
+const select=(state)=>{
+    return Object.assign({},state)
+}
+
+const Main=connect(
+
+ select
+
+)(Index)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Main / >      
+    </Provider> ,
     document.getElementById('app')
 );
+
+
